d87c9756d09b21ea0a13126972985d9beb30bb9a
Bikal Gurung <gbikal@gmail.com>
2025-11-23 12:22:54 +0000

    feat: Refactor FFI to use named Web API bindings, introduce dedicated Web API type definitions, and remove target-specific FFI files.

--- ffi.mjs
+++ ffi.mjs
@@ -3,11 +3,37 @@ export const importObject = {
         undefined: () => undefined,
         null: () => null,
     },
-    WebAPI_HTMLButtonElement: {
+    JsNull: {
+        null: () => null,
+    },
+    webapi_HTMLButtonElement: {
         new: (label) => {
             let button = document.createElement('button')
             button.textContent = label
             return button
         },
+    },
+    webapi_Document: {
+        new: () => document,
+        body: (document) => document.body,
+        querySelector: (document, selector) => document.querySelector(selector),
+    },
+    webapi_HTMLBodyElement: {
+        appendChild: (body, child) => body.appendChild(child),
+    },
+    webapi_AddEventListenerOptions: {
+        new: (capture, passive, once, signal) => ({ capture, passive, once, signal }),
+    },
+    webapi_Dictionary: {
+        empty: () => ({}),
+    },
+    webapi_EventTarget: {
+        addEventListener: (obj, type, listener, options) => obj.addEventListener(type, listener, options),
+    },
+    webapi_EventListenerOptions: {
+        new: (capture) => ({ capture }),
+    },
+    webapi_RemoveEventListenerOptionsArg: {
+        removeEventListener: (obj, type, listener, options) => obj.removeEventListener(type, listener, options),
     },
 };
